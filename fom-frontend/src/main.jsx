c933e1da34f481fd082bc43e87de08fa0b0dcf76
rpeter0 <peterbsa12@gmail.com>
2026-07-16 14:20:16 +0200

    add tab title and icon

--- fom-frontend/src/main.jsx
+++ fom-frontend/src/main.jsx
@@ -4,6 +4,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import logo from './assets/favicon-copia.png'
+
+document.title = "FOM Scheduler";
+
+const updateFavicon = (newIconPath) => {
+  let link = document.querySelector("link[rel*='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+  }
+  link.href = newIconPath;
+};
+updateFavicon(logo);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
